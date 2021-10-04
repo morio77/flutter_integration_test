a36a5b875ddf080bde4b248705289942e78c9363
morio77 <howtostudyy77@gmail.com>
2021-10-04 23:53:42 +0900

    簡単なインテグレーションテストを作成

---
+++ integration_test/main_view_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_integration_test/main.dart' as app;
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets("ボタンを押したときの挙動", (WidgetTester tester) async {
+    // アプリを立ち上げる
+    app.main();
+    await tester.pumpAndSettle(); // 描画が終わるのを待つ
+
+    // 初期値は'0'であることを確認
+    expect(find.text('0'), findsOneWidget);
+
+    // 1回ボタンタップしたら'1'であることを確認
+    await tester.tap(find.byKey(const Key('button')));
+    await tester.pumpAndSettle();
+    expect(find.text('1'), findsOneWidget);
+
+    // その後20回タップしたら'101'になっていることを確認
+    for (var i = 0; i < 100; i++) {
+      await tester.tap(find.byKey(const Key('button')));
+      await tester.pumpAndSettle();
+    }
+    expect(find.text('21'), findsOneWidget);
+  });
+}
